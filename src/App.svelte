c6dc601c7f8c04f24f4596233785b575c0b765a2
ampacheco <ampacheco@users.noreply.github.com>
2021-03-30 20:39:20 -0400

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Profile from "./Profile.svelte";
+	import { onMount} from "svelte";
+	
+	let members = [];
+	
+	onMount( async() => {
+			const res = await fetch(`https://raw.githubusercontent.com/ampacheco/svelte-demo-data/main/paypalmafia.json`);
+			members = await res.json();
+			console.log(members);
+	});
+	
+</script>
+
+<Profile>
+	<span slot="name">Bob</span>
+	<span slot="email">bob@email.com</span>
+</Profile>
+
+<Profile>
+	<span slot="name">Alice</span>
+	<span slot="phone">12345678</span>
+</Profile>
