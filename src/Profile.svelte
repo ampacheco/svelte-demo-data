a69f5963494d87e9a9c7115a60d1bdd434229a04
ampacheco <ampacheco@users.noreply.github.com>
2021-03-30 21:08:05 -0400

    Create Profile.svelte

---
+++ src/Profile.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let name;
+	export let title=null;
+	export let mail;
+	
+</script>
+
+<style>
+	section {
+		width: 350px;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		padding: 16px;
+		box-shadow: 2px 2px 4px #dedede;
+		border: 1px solid #888;
+		margin-bottom: 16px;
+	}
+</style>
+
+<section>
+	<div>Name</div>
+	<div>{name}</div>
+	{#if title}
+	<div>Title</div>
+	<div>{title}</div>
+	{/if}
+	<div>Email</div>
+	<div>{mail}@paypal@mafia</div>
+</section>
