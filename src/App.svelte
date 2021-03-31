7c1ae1e752c36c35471b9884054b0728d59a9ff0
ampacheco <ampacheco@users.noreply.github.com>
2021-03-30 21:08:40 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,17 @@
 <script>
+	import { onMount } from 'svelte'
 	import Profile from "./Profile.svelte";
-	import { onMount} from "svelte";
 	
 	let members = [];
 	
-	onMount( async() => {
-			const res = await fetch(`https://raw.githubusercontent.com/ampacheco/svelte-demo-data/main/paypalmafia.json`);
-			members = await res.json();
-			console.log(members);
+	onMount( async () => {
+		const res = await fetch(`https://raw.githubusercontent.com/ampacheco/svelte-demo-data/main/paypalmafia.json`);
+		members = await res.json();
+		
+		console.log(members);
 	});
-	
 </script>
 
-<Profile>
-	<span slot="name">Bob</span>
-	<span slot="email">bob@email.com</span>
-</Profile>
-
-<Profile>
-	<span slot="name">Alice</span>
-	<span slot="phone">12345678</span>
-</Profile>
+{#each members as member}
+   <Profile name={member.name} title={member.title} mail={member.mail} />
+{/each}
